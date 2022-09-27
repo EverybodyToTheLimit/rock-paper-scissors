88adad7bd108b0dcaa70c5fe25bbcc330a24704d
EverybodyToTheLimit <bartoszzawlocki@gmail.com>
2022-09-27 23:25:38 +0100

    Add game function, validation of prompt and cancel press behaviour

--- scripts/game-script.js
+++ scripts/game-script.js
@@ -19,3 +19,22 @@ function playRound (computerSelection, playerSelection) {
         return gameResult;
     }
 }
+
+function game() {
+    alert("Welcome to Rock, Paper and Scissors. This game will take your through 5 rounds of the game. Each round you will be asked to enter Rock, Paper or Scissors. See if you can beat the computer! Good luck!");
+    for(let i=1; i<6; i++) {
+        try {    
+            let userSelection = prompt("Round " + i + " please enter rock, paper or scissors!").toLowerCase;
+            while ((userSelection !== "rock") && (userSelection !== "paper") && (userSelection !== "scissors")) 
+            {
+                userSelection = prompt("Wrong input. Please only enter: rock, paper or scissors");
+            }
+        } catch (e) {
+            if (e instanceof TypeError) {
+                alert("Thanks for playing!!");
+                break
+              }
+        }    
+    }
+}
+
