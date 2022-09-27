f5c5f57d18e68bb39d254e2518645a626f31e9d4
EverybodyToTheLimit <bartoszzawlocki@gmail.com>
2022-09-27 21:27:43 +0100

    Add computerChoice function

---
+++ scripts/game-script.js
@@ -0,0 +1,6 @@
+function computerChoice () {
+    arr = ["rock", "paper", "scissors"];
+    let randomIndex = Math.floor(Math.random() * arr.length);
+    let randomItem = arr[randomIndex];
+    return randomItem;
+}
