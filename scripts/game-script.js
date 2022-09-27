411f4ecbad641c53eada3b66c0d1a4d8223ff22b
EverybodyToTheLimit <bartoszzawlocki@gmail.com>
2022-09-27 21:58:56 +0100

    Add playRound function, compare input lowercase

--- scripts/game-script.js
+++ scripts/game-script.js
@@ -4,3 +4,18 @@ function computerChoice () {
     let randomItem = arr[randomIndex];
     return randomItem;
 }
+
+function playRound (computerSelection, playerSelection) {
+    if (playerSelection.toLowerCase() == computerSelection) {
+        let gameResult = "It's a stalemate! You both drew " + playerSelection;
+        return gameResult;
+    }
+    else if ((playerSelection.toLowerCase() == "rock" && computerSelection == "paper") || ((playerSelection.toLowerCase() == "paper" && computerSelection == "scissors")) || (playerSelection.toLowerCase() == "scissors" && computerSelection == "rock")) {
+        let gameResult = "You loose! " + computerSelection + " beats " + playerSelection;
+        return gameResult;
+    }
+    else {
+        let gameResult = "You win! " + playerSelection + " beats " + computerSelection;
+        return gameResult;
+    }
+}
