8889b466e4cc9ea017057a1e27dec578073dff78
EverybodyToTheLimit <bartoszzawlocki@gmail.com>
2022-09-28 10:28:59 +0100

    Add final results if statements

--- scripts/game-script.js
+++ scripts/game-script.js
@@ -39,23 +39,41 @@ function game() {               //main game function
     for(let i=1; i<6; i++) {
         try {               //try catch to capture Cancel button
             let userSelection = prompt("Round " + i + " please enter rock, paper or scissors!");
-            while (validateInput(userSelection) == false)  {
-                alert("Incorrect input, please ONLY enter rock, paper or scissors!");
-                i --;
-                break;
-            }
             
+            if (validateInput(userSelection) == false) {
+                while (validateInput(userSelection) == false)  {
+                    alert("Incorrect input, please ONLY enter rock, paper or scissors!");
+                    i --;
+                    break;
+                }
+            }
+            else {
 
                 let result = (playRound(computerChoice(), userSelection));
-                if (result.slice(0,5) == "You l") {
-                    computerScore ++;  
-                }
-                else  if (result.slice(0,5) == "You w") {
-                    userScore ++;      
-                }
-                console.log(result);
-                console.log("The score is \nComputer: " + computerScore + "\nUser: " + userScore);
-
+                    if (result.slice(0,5) == "You l") {
+                        computerScore ++;  
+                    }
+                    else  if (result.slice(0,5) == "You w") {
+                        userScore ++;      
+                    }
+                
+                    if (i<5) {
+                        console.log(result);
+                        console.log("The score is \nComputer: " + computerScore + "\nUser: " + userScore);
+                    }
+                    else if (i==5 && (computerScore>userScore)) {
+                        console.log(result);
+                        console.log("The final score is ***DRUMROLL*** \nComputer: " + computerScore + "\nUser: " + userScore +"\nBetter luck next time!");
+                    }
+                    else if (i==5 && (computerScore<userScore)) {
+                        console.log(result);
+                        console.log("The final score is ***DRUMROLL*** \nComputer: " + computerScore + "\nUser: " + userScore +"\nCongratulations! Well played!");
+                    }
+                    else if (i==5 && (computerScore=userScore)) {
+                        console.log(result);
+                        console.log("The final score is ***DRUMROLL*** \nComputer: " + computerScore + "\nUser: " + userScore +"\nNeck in neck!What a game!");
+                    }
+            }
 
         } catch (e) {
             if (e instanceof TypeError) {
